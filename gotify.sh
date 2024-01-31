746d39066256d1ae1a38042ef67aebb34957696a
Miralthor <55807386+Miralthor@users.noreply.github.com>
2024-01-31 16:00:56 +0100

    Update gotify.sh
    URL is now separate from the curl command and can be adjusted more easily in the script. The zabbix problem levels have been added so that they can be used for gotify priority.

--- gotify.sh
+++ gotify.sh
@@ -1,7 +1,38 @@
 #!/bin/bash
 
-ALERTSENDTO=$1
-ALERTSUBJECT=$2
-ALERTMESSAGE=$3
+# Gotify-Server URL (IP or FQDN)
+GOTIFY_URL="http://change.me"
 
-curl -X POST "http://your-domain.com/message?token=$ALERTSENDTO" -F "title=$ALERTSUBJECT" -F "message=$ALERTMESSAGE" -F "priority=5" > /dev/null 2>&1
+# Zabbix Parameter
+GOTIFY_TOKEN=$1
+SUBJECT=$2
+MESSAGE=$3
+SEVERITY=$4
+
+# Converting Zabbix issue level to Gotify priority
+case $SEVERITY in
+    "Disaster")
+        PRIORITY=10
+        ;;
+    "High")
+        PRIORITY=8
+        ;;
+    "Average")
+        PRIORITY=5
+        ;;
+    "Warning")
+        PRIORITY=3
+        ;;
+    "Information")
+        PRIORITY=0
+        ;;
+    "Not classified")
+        PRIORITY=0
+        ;;
+    *)
+        PRIORITY=1  # Default value for undefined severity
+        ;;
+esac
+
+# Sending the message to Gotify
+curl -X POST "$GOTIFY_URL/message?token=$GOTIFY_TOKEN" -F "title=$SUBJECT" -F "message=$MESSAGE" -F "priority=$PRIORITY"
